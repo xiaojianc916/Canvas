59c157164c7764d6bbdb5b94beeac559c99df6cb
xiaojianc916 <xiaojianc125@gmail.com>
2026-07-21 22:33:36 +0800

    优化了相关问题

--- apps/desktop/src-tauri/src/commands/mod.rs
+++ apps/desktop/src-tauri/src/commands/mod.rs
@@ -1,13 +1,4 @@
-pub mod asset;
 pub mod file;
 pub mod opener;
-pub mod plugin;
 pub mod settings;
 pub mod window;
-
-pub use asset::*;
-pub use file::*;
-pub use opener::*;
-pub use plugin::*;
-pub use settings::*;
-pub use window::*;
